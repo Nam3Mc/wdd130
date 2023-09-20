96f29d5ea059a70cb612fd77eb254e533884b624
Nam3Mc <108500975+Nam3Mc@users.noreply.github.com>
2023-09-19 19:48:23 -0500

    Delete pce 111 directory

--- pce 111/boxes.py
+++
@@ -1,9 +0,0 @@
-
-print ( 'To help you calculating boxes needed, please!')
-itens = int ( input ('Enter the number of items:' ) )
-itens_x_boxes = int ( input ( 'Enter the number of items per box: ' ) )
-import math
-
-boxes_needed = ( itens / itens_x_boxes ) 
-
-print ( f' For {itens} items, packing {itens_x_boxes} items in each box, you will need {math.ceil(boxes_needed)} boxes.' )
