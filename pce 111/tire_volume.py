8cca5ac02a3a880ad634fe0d798599ba4dd75e6a
Nam3Mc <mor23083@byui.edu>
2023-09-24 16:17:29 -0500

    les's try

--- pce 111/tire_volume.py
+++
@@ -1,9 +0,0 @@
-import math
-
-width = int ( input ( ' Enter the width of the tire in mm (ex 205): ' )  )
-aspect_ratio = int ( input ( ' Enter the aspect ratio of the tire (ex 60): ' )  )
-diameter = int ( input ( ' the diameter of the wheel in inches (ex 15): ' )  )
-
-tire_volume = ( math.pi * (width**2) *  aspect_ratio * ( width * aspect_ratio + 2540 * diameter ) ) / 10000000000
-
-print ( f'The approximate volume is {tire_volume:.2f}')
